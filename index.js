546ef9151f01c7f7fc88c7591d8188b8e0efedc4
JOE_WALKER <josephalexanderwalker@gmail.com>
2021-02-04 16:14:48 +0000

    index.js port change

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var http = require("http");
 
+const $PORT;
+
 http.createServer(function (request, reponse){
     reponse.writeHead(200, {'Content-Type': 'text/plain'});
     reponse.end('Hello World\n');
-}).listen(8080);
+}).listen($PORT);
 
-console.log('Server running at http://127.0.0.1:8080/');
+console.log('Server running at http://127.0.0.1:$PORT/');
