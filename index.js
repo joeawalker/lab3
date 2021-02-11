7724e7b8c92aa8e44b34ad5a9879ed316325ccfa
JOE_WALKER <josephalexanderwalker@gmail.com>
2021-02-11 20:37:01 +0000

    indexUpdate

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+const { response } = require("express");
+const { ReadStream } = require("fs");
 var http = require("http");
+
+fs = require('fs');
 
 const PORT = process.env.PORT || 3000;
 //app.listen(PORT, () => {
@@ -6,8 +10,10 @@ const PORT = process.env.PORT || 3000;
 //});
 
 http.createServer(function (request, reponse){
-    reponse.writeHead(200, {'Content-Type': 'text/plain'});
-    reponse.end('Hello World\n');
+    reponse.writeHead(200, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin' : '*'});
+    var readSteam = fs.createReadStream(__dirname + '/index.html');
+    ReadStream.pipe(response);
+    //reponse.end('Hello World\n');
 }).listen(PORT);
 
 console.log('Server running at http://127.0.0.1:'+PORT+'/');
